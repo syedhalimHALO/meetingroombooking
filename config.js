7db8635bfac57fb77cbbe6fac63e0743070808ae
syedhalimHALO <halim.syed@winner.com.my>
2026-08-18 16:34:34 +0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.APP_CONFIG = {
-  supabaseUrl: 'https://hizzowxftuzafapzqkza.supabase.co',
-  supabaseAnonKey: 'sb_publishable_jk7fKrOyvfGeA0z0BjBM8w_bkR6JF4X',
-  roomSlug: 'marketing-meeting-room',
-  timezone: 'Asia/Kuala_Lumpur',
-  enableAzureLogin: false,
-  allowedEmailDomain: ''
-};
